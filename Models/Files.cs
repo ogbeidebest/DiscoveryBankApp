af28bb88efb4bf38a4824961d1e72f098bd898ea
Best Ogbeide <ogbeidebest.ob@gmail.com>
2022-08-03 19:52:25 +0100

    "Initial commit"

---
+++ Models/Files.cs
@@ -0,0 +1,19 @@
+﻿namespace Models
+{
+    public class Files
+    {
+        // string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+        //public static string UserLogin = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/../LoginFile.txt";
+        //public static string CustomerPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/../UsersFile.txt";
+        //public static string TransactionPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/../TransactionsFile.txt";
+        //public static string accountPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/../AccountFile.txt";
+        //public static string LoginSessionPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/../LoginSession.txt";
+
+        public static string UserLogin = @"C:\Users\hp\OneDrive\Desktop\week-4-task-ogbeidebest\DiscoveryBank\DataStore\DataStorage\LoginFile.txt";
+        public static string CustomerPath = @"C:\Users\hp\OneDrive\Desktop\week-4-task-ogbeidebest\DiscoveryBank\DataStore\DataStorage\UsersFile.txt";
+        public static string TransactionPath = @"C:\Users\hp\OneDrive\Desktop\week-4-task-ogbeidebest\DiscoveryBank\DataStore\DataStorage\TransactionsFile.txt";
+        public static string accountPath = @"C:\Users\hp\OneDrive\Desktop\week-4-task-ogbeidebest\DiscoveryBank\DataStore\DataStorage\AccountFile.txt";
+        public static string LoginSessionPath = @"C:\Users\hp\OneDrive\Desktop\week-4-task-ogbeidebest\DiscoveryBank\DataStore\DataStorage\LoginSession.txt";
+    }
+}
